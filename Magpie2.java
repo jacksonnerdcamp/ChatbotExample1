fccbf6c37ff5ffec22880548109e93d9c8e3d802
BT_1N3_27 <jacksonjjacobs@gmail.com>
2018-10-30 08:43:38 -0400

    updatwe

--- Magpie2.java
+++ Magpie2.java
@@ -42,6 +42,22 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("cat") >= 0 || statement.indexOf("dog") >= 0)
+		{
+			response = "Tell me more about your pets!";
+		}
+		else if (statement.indexOf("Mr.") >= 0)
+		{
+			response = "He sounds great!";
+		}
+		else if (statement.indexOf("Mrs.") >= 0)
+		{
+			response = "She sounds great!";
+		}
+		else if (statement.equals(""))
+		{
+			response = "bleh";
+		}
 		else
 		{
 			response = getRandomResponse();
